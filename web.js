beb9604651369a03e970a8d2a08c3311af4d8c18
Matteo <mat90c@gmail.com>
2013-07-09 07:51:06 +0000

    test

---
+++ web.js
@@ -0,0 +1,16 @@
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+var fs = require('fs');
+
+app.get('/', function(request, response) {
+  var content = fs.readFileSync("index.html");
+  response.send(content);
+});
+
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
