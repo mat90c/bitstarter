2f295e9ffd444779e8df9554f27b28cecdec4f92
Matteo <mat90c@gmail.com>
2013-07-09 07:58:22 +0000

    web.js

--- web.js
+++ web.js
@@ -2,7 +2,6 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-<<<<<<< HEAD
 var fs = require('fs');
 
 app.get('/', function(request, response) {
@@ -15,13 +14,3 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-=======
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
->>>>>>> 93f0c258e90afebc3670e7a63f14dafb20a9036a
